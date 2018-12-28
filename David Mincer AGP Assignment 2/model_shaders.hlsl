b07f6df19b8029918bc40e9f1f203f0cfa67a1fe
David Mincer <david.r.mincer@live.com>
2018-12-28 18:31:48 +0000

    All cubes replaced with Models

--- David Mincer AGP Assignment 2/model_shaders.hlsl
+++ David Mincer AGP Assignment 2/model_shaders.hlsl
@@ -1,9 +1,9 @@
 cbuffer CB0
 {
 	matrix WVPMatrix;					// 64 bytes
-	float4 directional_light_vector;	// 16 bytes
-	float4 directional_light_colour;	// 16 bytes
-	float4 ambient_light_colour;		// 16 bytes
+	//float4 directional_light_vector;	// 16 bytes
+	//float4 directional_light_colour;	// 16 bytes
+	//float4 ambient_light_colour;		// 16 bytes
 										//TOTAL SIZE = 112 bytes
 };
 
@@ -21,13 +21,18 @@ VOut ModelVS(float4 position : POSITION, float2 texcoord : TEXCOORD, float3 norm
 {
 	VOut output;
 
-	output.position = mul(WorldViewProjection, position);
+	/*output.position = mul(WorldViewProjection, position);
 
 	float diffuse_amount = dot(directional_light_vector, normal);
 	diffuse_amount = saturate(diffuse_amount);
 
 	output.color = ambient_light_colour + (directional_light_colour * diffuse_amount);
+	output.texcoord = texcoord;*/
+
+	float4 default_color = { 1.0, 1.0, 1.0, 1.0 };
+	output.position = mul(WVPMatrix, position);
 	output.texcoord = texcoord;
+	output.color = default_color;
 
 	return output;
 }
